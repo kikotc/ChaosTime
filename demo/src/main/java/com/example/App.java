cb949589a4f273f379f471f4393bc429d0e38420
leowang19 <leowang220503@gmail.com>
2023-12-06 14:37:05 -0500

    Title :dog:
    Titled the Menu Screen created button object and imported necessary libraries

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,12 +16,20 @@ import java.io.IOException;
 public class App extends Application {
     //declare at class scope 
     public static Scene scene;
+    public Pane MenuLayout = new Pane();
+    public Pane game = new Pane();
+    public Stage stage = new Stage();
+    public Button Start = new Button("Start");
+
     
 
     @Override
     public void start(Stage stage) throws IOException {
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("Chaos Time");
+
+        
     }
 
     public static void main(String[] args) {
