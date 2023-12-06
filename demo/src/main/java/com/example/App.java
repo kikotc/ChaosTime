6bd93214593654d21341c74db423fe2073c982b1
leowang19 <leowang220503@gmail.com>
2023-12-06 14:55:22 -0500

    Created Button and Maxed out Scene Size
    found some code on stack overflow to find the max size of screen and set stage size to be said size https://stackoverflow.com/questions/31426912/how-to-make-window-fullscreen-maximized-in-scene-builder

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -2,10 +2,13 @@ package com.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,20 +17,35 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-    //declare at class scope 
+    //declare variables at class scope 
     public static Scene scene;
     public Pane MenuLayout = new Pane();
     public Pane game = new Pane();
     public Stage stage = new Stage();
     public Button Start = new Button("Start");
+    public Scene menuScene = new Scene(MenuLayout);
+    
 
     
 
     @Override
     public void start(Stage stage) throws IOException {
-        stage.setScene(scene);
+        //gets the max size of screen and stores in a variable 
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        //makes the Stage be created at the max size of user screen 
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
         stage.show();
         stage.setTitle("Chaos Time");
+        stage.setScene(menuScene);
+        MenuLayout.getChildren().add(Start);
+        Start.relocate(400, 500);
+        
+        
+
 
         
     }
